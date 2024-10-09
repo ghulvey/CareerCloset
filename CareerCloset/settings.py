6e003ee24a7456f46e82b6cc4ed188cc01083ddf
Gagandeep Dhami <125837163+GagandeepDhami@users.noreply.github.com>
2024-10-09 15:09:03 -0400

    home.html & home.css update

--- CareerCloset/settings.py
+++ CareerCloset/settings.py
@@ -15,6 +15,18 @@ import CareerCloset.secrets as secrets
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# settings.py
+import os
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
